c87f56ede254b13c0057013f2710f1f613f81015
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2025-08-06 12:03:26 -0700

    Add probes to NpexepUploadQueue

--- src/np_jobs/queues/sqlite_isilon/pipeline_npexp_upload_queue.py
+++ src/np_jobs/queues/sqlite_isilon/pipeline_npexp_upload_queue.py
@@ -3,3 +3,7 @@ from np_jobs.queues.sqlite_isilon.base import SqliteIsilonJobQueue
 class PipelineNpexpUploadQueue(SqliteIsilonJobQueue):
     
     table_name = 'npexp_upload'
+    column_definitions = dict(
+        **SqliteIsilonJobQueue.column_definitions,
+        probes='TEXT NOT NULL DEFAULT ABCDEF',
+    )
